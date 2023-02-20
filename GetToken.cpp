7f1124ec4b5186afc64df4478022cb3f60f66436
gaxzipTGO <95045499+gaxzipTGO@users.noreply.github.com>
2023-02-20 20:12:08 +0800

    Add files via upload

--- GetToken.cpp
+++ GetToken.cpp
@@ -8,14 +8,28 @@
 using namespace std ;
 
 class GetTokenMachine {
+    protected: bool isFile ;
     protected: fstream fin ;
-    protected: string token = "" ;
+    protected: string token ;
     protected: bool end = false ;
     protected: char nextChar = '\0' ; // used to cheak
     protected: char bufferDelimiter = '\0' ; // used to save delimiter
 
+    public: GetTokenMachine() {
+        try{
+            isFile = false ;
+            token = "" ;
+        }
+        catch( exception &e ) {
+            cerr << e.what() << " ," << "in line : " << __LINE__ << endl ; 
+            throw invalid_argument(e.what())  ;            
+        }
+    }
+
     public: GetTokenMachine(string file) {
         try {
+            isFile = true ;
+            token = "" ;
             this->fin.open(file) ;
             if ( !fin ) {
                 throw invalid_argument("the file not open")  ;
@@ -37,6 +51,30 @@ class GetTokenMachine {
             return true ;
         }
         return false ;
+    }
+
+    protected: bool GetToken( string & token ) {
+        // if read delimite we need to save the token and return
+        bool isEmpty = false ;
+        if ( bufferDelimiter != '\0' ) {
+            token = token + string(1,bufferDelimiter) ;
+            bufferDelimiter = '\0' ;
+            return isEmpty ;
+        } // this means our buffer have a delimiter we need to use it first 
+        cin.get(nextChar) ;
+        while ( nextChar != '\t' &&nextChar !=' ' && nextChar != '\n' && ! IsDelimiter(nextChar) && ! cin.eof() ) {
+            token = token + string(1,nextChar) ;
+            cin.get(nextChar) ;
+        }
+        if ( IsDelimiter(nextChar) ) {
+            bufferDelimiter = nextChar ;
+        } // if nextChar is delimiter, it will leave while and go to here
+
+        if ( ! cin.eof() ) {
+            return isEmpty  ;
+        }
+        else return true ;
+
     }
 
     protected: bool GetFileToken( fstream &file,string & token ) {
@@ -63,19 +101,6 @@ class GetTokenMachine {
 
     }
 
-    public: void Test() {
-        char continueTime = 'y' ;
-        while ( continueTime == 'y' && ! end ) {
-            end = GetFileToken(fin,this->token) ;
-            if ( token.length() > 0 ) {
-                cout << token << endl ;
-                token = "" ;             
-            }
-            //cout << "do yo countinue?" << endl ;
-            //cin >> continueTime ; 
-        } 
-        cin >> token ;
-    }
 
     public: bool GetNextToken(string &Out_token) {
         /*
@@ -84,7 +109,12 @@ class GetTokenMachine {
         token = "" ;
         if ( ! end ) {
             do {
-                end = GetFileToken(fin,token) ;
+                if ( isFile == true ) {
+                    end = GetFileToken(fin,token) ;
+                }
+                else {
+                    end = GetToken(token) ;
+                } 
                 if( end == true ) {
                     Out_token = token ; 
                     return false ;
@@ -107,23 +137,19 @@ class GetTokenMachine {
         else return false ;
     }
 
-} ;
+} getTokenMachine;
 
-/*
-用法為:
-    string fileName ;
-    cin >> fileName ;
-    string token ;
+int main() {
+    string token = "" ;
     bool end ;
-    GetTokenMachine machine1( fileName ) ;
     do {
-        end = machine1.GetNextToken(token) ; // end is a bool means the page is EOF if end is false
+        end = getTokenMachine.GetNextToken(token) ; // end is a bool means the page is EOF if end is false
         if ( token != "" ) {
             // here we can put some choise to get token like's string
             cout << token << ' ';
         } // the compare is very important!!! that can cheak the token does read something   
-        if ( machine1.IsEnterChar() ) {
+        if ( getTokenMachine.IsEnterChar() ) {
             cout << '\n' ;
         }     
-    } while ( end ) ;   // this while is we can loading all token of page
-*/
+    } while ( end ) ;   // this while is we can loading all token of page    
+}
